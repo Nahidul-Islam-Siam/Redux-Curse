1d334a1278196e24bb81fbf74051a458b03c4fdb
Nahidul Islam Siam <siamnahidul093@gmail.com>
2025-02-03 21:54:24 +0600

    add todo

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+
+
 import './index.css'
-import App from './App.jsx'
+import { Provider } from 'react-redux'
+import store from './Redux/store.js'
+
+
+import Counter from './Counter.jsx'
+import { createRoot } from 'react-dom/client'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <Provider store= {store}>
+<Counter/>
+  </Provider>,
 )
